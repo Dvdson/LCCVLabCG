6e935687c5fdb583f0bfc11b6a5701d162e5f62c
Dvdson <dvdsoncomp@gmail.com>
2016-08-27 17:50:28 -0300

    maked object

---
+++ Object.h
@@ -0,0 +1,24 @@
+//
+// Created by dvdson on 8/27/16.
+//
+
+#ifndef LCCVLABCG_OBJECT_H
+#define LCCVLABCG_OBJECT_H
+
+#include <GL/glut.h>
+
+
+class Object {
+private:
+    GLfloat posX;
+    GLfloat posY;
+    GLfloat posZ;
+
+public:
+    bool setPosition(GLfloat X, GLfloat Y, GLfloat Z);
+    virtual void drawObj(void);
+
+};
+
+
+#endif //LCCVLABCG_OBJECT_H
