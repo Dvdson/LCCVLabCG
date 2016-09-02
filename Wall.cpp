cf151019eec702337425a1b5bd6cdae274a1d5e3
Dvdson <dvdsoncomp@gmail.com>
2016-09-02 18:31:03 -0300

    make  camera controler with keyboard(not angle control yet)

--- Wall.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by dvdson on 8/27/16.
-//
-
-#include "Wall.h"
--- src/main.cpp
+++ src/main.cpp
@@ -8,132 +8,145 @@ using namespace std;
 
 int winWidth = 1280, winHeight = 720;
 
-GLfloat QPosx=0, QPosy=0, QPosz=0;
-GLfloat keySencitivity = 5;
-GLdouble camPosX=0, camPosY=0, camPosZ=3.0;
-GLdouble camSencitivity=5;
+bool firstT = true;
+
+GLfloat QPosX=0, QPosY=0, QPosZ=0;
+GLfloat keySencitivity = 1;
+GLdouble camPosX=0, camPosY=0, camPosZ=1;
+GLdouble camSencitivity=1;
+
 
 void init(void) {
-    glEnable(GL_BLEND);
+    glClearColor(0.0, 0.0, 0.0, 0.0);
 
     glEnable(GL_DEPTH_TEST);
     glClearDepth(1.0);
 
-    glClearColor(0.0, 0.0, 0.0, 0.0);
+//      glOrtho(0.0, winWidth, 0.0, winHeight, -100, 100);
+    glViewport(0,0,(GLsizei) winWidth, (GLsizei) winHeight);
 
-    //glMatrixMode(GL_PROJECTION);
-
-    glOrtho(0.0, winWidth, 0.0, winHeight, -5000.0, 5000.0);
-}
-
-
-void displayRoom(void){
-
-    //Some predefinitions
-    GLfloat proporX, proporY, thicknessWall,
-    //The Walls especified in coments
-
-
-    glPushMatrix();
-        glTranslatef(QPosx,QPosy,QPosz);
-        drawHexahedron(500,300,1);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslatef(QPosx,QPosy,QPosz);
-        drawHexahedron(500,300,1);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslatef(QPosx,QPosy,QPosz);
-        drawHexahedron(500,300,1);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslatef(QPosx,QPosy,QPosz);
-        drawHexahedron(500,300,1);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslatef(QPosx,QPosy,QPosz);
-        drawHexahedron(500,300,1);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslatef(QPosx,QPosy,QPosz);
-        drawHexahedron(500,300,1);
-    glPopMatrix();
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
 
 }
 
 void display(void) {
 
-    //float x = 320, y = 180, z = .0;
-    gluLookAt(camPosX ,camPosY,camPosZ,0.0,0.0,.0,0.0,1.0,0.0);
+    glClear(GL_COLOR_BUFFER_BIT
+            | GL_DEPTH_BUFFER_BIT
+    );
 
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    //glMatrixMode(GL_MODELVIEW);
+    //glLoadIdentity();
+    gluLookAt(camPosX, camPosY, camPosZ, camPosX, camPosY, camPosZ-1, 0, 1, 0);
+    camPosX = 0, camPosY = 0, camPosZ = 0;
 
+
+
+//    glMatrixMode(GL_PROJECTION);
+//    glLoadIdentity();
     //floor
+    glPushMatrix();
+        glTranslatef(QPosX,QPosY,QPosZ);
+        glColor4f(1,1,1,1);
+        drawHexahedron(1,1,1);
+    glPopMatrix();
 
-
-    displayRoom();
+    //displayRoom();
     //drawPlan(x,y,z,winWidth,winHeight);
 
     glutSwapBuffers();
 }
 
 void reshape(GLint w, GLint h){
+
     glViewport(0,0,(GLsizei) w, (GLsizei) h);
 
     glMatrixMode(GL_MODELVIEW);
-    //glLoadIdentity();
+    glLoadIdentity();
 
-    //gluPerspective(60,(GLfloat) w/(GLfloat) h,2,1);
+    gluPerspective(60.0,(GLfloat) w/(GLfloat) h,1.0,200.0);
+    gluLookAt(camPosX,camPosY,camPosZ,camPosX,camPosY,camPosZ-1,0,1,0);
 
-
+    glutPostRedisplay();
 }
 
 void keyboard(unsigned char key, int x, int y) {
     switch(key)
     {
         case 'w':
-            QPosy += keySencitivity;
+            camPosY += keySencitivity;
+            //glutPostRedisplay();
             break;
         case 's':
-            QPosy -= keySencitivity;
+            camPosY -= keySencitivity;
+            //glutPostRedisplay();
             break;
         case 'a':
-            QPosx -= keySencitivity;
+            QPosX -= keySencitivity;
+            //glutPostRedisplay();
             break;
         case 'd':
-            QPosx += keySencitivity;
+            QPosX += keySencitivity;
+            //glutPostRedisplay();
+            break;
+        case 'r':
+            QPosZ -= keySencitivity;
+            //glutPostRedisplay();
+            break;
+        case 'f':
+            QPosZ += keySencitivity;
+            //glutPostRedisplay();
             break;
         case '1':
-            keySencitivity -= 5;
+            keySencitivity -= 0.1;
+            //glutPostRedisplay();
             break;
         case '2':
-            keySencitivity += 5;
+            keySencitivity += 0.1;
+            //glutPostRedisplay();
             break;
         case 'i':
             camPosY += camSencitivity;
+            //glutPostRedisplay();
+            break;
+        case 'k':
+            camPosY -= camSencitivity;
+            //glutPostRedisplay();
             break;
         case 'j':
-            camPosY -= camSencitivity;
-            break;
-        case 'h':
             camPosX -= camSencitivity;
+            //glutPostRedisplay();
             break;
         case 'l':
             camPosX += camSencitivity;
+            //glutPostRedisplay();
+            break;
+        case 'y':
+            camPosZ -= camSencitivity;
+            //glutPostRedisplay();
+            break;
+        case 'h':
+            camPosZ += camSencitivity;
+            //glutPostRedisplay();
             break;
         case '3':
-            camSencitivity -= 5;
+            camSencitivity -= 0.1;
+            cout << camSencitivity << endl;
+            //glutPostRedisplay();
             break;
         case '4':
-            camSencitivity += 5;
+            camSencitivity += 0.1;
+            cout << camSencitivity << endl;
+            //glutPostRedisplay();
+            break;
+        default:
             break;
     }
-
+    cout << "( " << to_string(camPosX) << endl;
+    cout << ", " << to_string(camPosY) << endl;
+    cout << ", " << to_string(camPosZ) << endl;
+    cout << ")" << endl;
     glutPostRedisplay();
 }
 
@@ -143,7 +156,7 @@ int main(int argc, char** argv) {
 
     glutInit(&argc,argv);
 
-    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+    glutInitDisplayMode(GLUT_DEPTH | GLUT_SINGLE | GLUT_RGBA);
     glutInitWindowSize(winWidth, winHeight);
     glutInitWindowPosition(200, 200);
     glutCreateWindow("Waves");
